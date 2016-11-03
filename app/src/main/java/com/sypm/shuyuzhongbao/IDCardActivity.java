74123db0607f54952e8d1ad12f175dbcc5841539
lax <1158152686@qq.com>
2016-11-03 16:25:48 +0800

    图片选择器完成

--- app/src/main/java/com/sypm/shuyuzhongbao/IDCardActivity.java
+++ app/src/main/java/com/sypm/shuyuzhongbao/IDCardActivity.java
@@ -1,17 +1,26 @@
 package com.sypm.shuyuzhongbao;
 
-import android.content.DialogInterface;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.sypm.shuyuzhongbao.utils.BaseActivity;
+import com.yuyh.library.imgsel.ImageLoader;
+import com.yuyh.library.imgsel.ImgSelActivity;
+import com.yuyh.library.imgsel.ImgSelConfig;
+
+import java.util.List;
 
 public class IDCardActivity extends BaseActivity {
 
-    ImageView imageView1, imageView2;
+    private static final int REQUEST_CODE = 0;
 
+    ImageView imageView1, imageView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +35,62 @@ public class IDCardActivity extends BaseActivity {
         imageView1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Single(imageView1);
             }
 
         });
         imageView2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Single(imageView1);
             }
         });
+    }
+
+    private ImageLoader loader = new ImageLoader() {
+        @Override
+        public void displayImage(Context context, String path, ImageView imageView) {
+            Glide.with(context).load(path).into(imageView);
+        }
+    };
+
+    public void Single(View view) {
+        ImgSelConfig config = new ImgSelConfig.Builder(loader)
+                // 是否多选
+                .multiSelect(true)
+                // 确定按钮背景色
+                .btnBgColor(Color.parseColor("#FF4081"))
+                // 确定按钮文字颜色
+                .btnTextColor(Color.WHITE)
+                // 使用沉浸式状态栏
+                .statusBarColor(Color.parseColor("#FF4081"))
+                // 返回图标ResId
+                .backResId(android.support.v7.appcompat.R.drawable.abc_ic_ab_back_material)
+                .title("身份证正反两张图片")
+                .titleColor(Color.WHITE)
+                .titleBgColor(Color.parseColor("#FF4081"))
+                .cropSize(1, 1, 200, 200)
+                .needCrop(false)
+                // 第一个是否显示相机
+                .needCamera(true)
+                // 最大选择图片数量
+                .maxNum(2)
+                .build();
+        // 跳转到图片选择器
+        ImgSelActivity.startActivity(this, config, REQUEST_CODE);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            List<String> pathList = data.getStringArrayListExtra(ImgSelActivity.INTENT_RESULT);
+
+            // 测试Fresco。可不理会
+            imageView1.setImageURI(Uri.parse("file://" + pathList.get(0)));
+            imageView2.setImageURI(Uri.parse("file://" + pathList.get(1)));
+
+        }
     }
 }
