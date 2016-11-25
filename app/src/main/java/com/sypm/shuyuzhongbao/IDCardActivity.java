53f60e295fffba2e017490da6c30245a8743903f
lax <1158152686@qq.com>
2016-11-25 10:24:16 +0800

    image

--- app/src/main/java/com/sypm/shuyuzhongbao/IDCardActivity.java
+++ app/src/main/java/com/sypm/shuyuzhongbao/IDCardActivity.java
@@ -45,7 +45,6 @@ public class IDCardActivity extends BaseActivity {
             }
 
         });
-
         imageView2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
