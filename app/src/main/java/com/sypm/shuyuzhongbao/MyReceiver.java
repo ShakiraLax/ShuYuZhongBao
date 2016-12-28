b10e2dcefd51ef1c5bb09fe61e9dc35f1d867070
lax <1158152686@qq.com>
2016-12-28 11:32:35 +0800

    修改提示音

--- app/src/main/java/com/sypm/shuyuzhongbao/MyReceiver.java
+++ app/src/main/java/com/sypm/shuyuzhongbao/MyReceiver.java
@@ -3,10 +3,11 @@ package com.sypm.shuyuzhongbao;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.sypm.shuyuzhongbao.utils.ExampleUtil;
 import com.sypm.shuyuzhongbao.utils.RememberHelper;
 
 import org.json.JSONException;
@@ -24,26 +25,34 @@ import cn.jpush.android.api.JPushInterface;
 public class MyReceiver extends BroadcastReceiver {
     private static final String TAG = "JPush";
 
+    final MediaPlayer mp = new MediaPlayer();
+
+
     @Override
     public void onReceive(Context context, Intent intent) {
         Bundle bundle = intent.getExtras();
-        Log.d(TAG, "[MyReceiver] onReceive - " + intent.getAction() + ", extras: " + printBundle(bundle));
+//        Log.d(TAG, "[MyReceiveronReceive] onReceive - " + intent.getAction() + ", extras: " + printBundle(bundle));
 
-        //保存registrationId:160a3797c80a1a27a63
+            /*NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+            builder.setSound(Uri.parse("android.resource://" + context.getPackageName() + "/" + R.raw.notify));
+            Notification notification = builder.build();
+            int notifyId = (int) System.currentTimeMillis();
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.notify(notifyId, notification);*/
 
-        if (JPushInterface.ACTION_REGISTRATION_ID.equals(intent.getAction())) {
-            String regId = bundle.getString(JPushInterface.EXTRA_REGISTRATION_ID);
-            Log.d(TAG, "[MyReceiver] 接收Registration Id : " + regId);
-            RememberHelper.saveRegistrationId(regId);
-            //send the Registration Id to your server...
-
-        } else if (JPushInterface.ACTION_MESSAGE_RECEIVED.equals(intent.getAction())) {
-            Log.d(TAG, "[MyReceiver] 接收到推送下来的自定义消息: " + bundle.getString(JPushInterface.EXTRA_MESSAGE));
-            Log.d(TAG, "[MyReceiver] 接收到推送下来的自定义消息: " + bundle.getString(JPushInterface.EXTRA_TITLE));
+        if (JPushInterface.ACTION_MESSAGE_RECEIVED.equals(intent.getAction())) {
+//            Log.d(TAG, "[MyReceiverACTION_MESSAGE_RECEIVED] 接收到推送下来的自定义消息: " + bundle.getString(JPushInterface.EXTRA_MESSAGE));
+//            Log.d(TAG, "[MyReceiver] 接收到推送下来的自定义消息: " + bundle.getString(JPushInterface.EXTRA_TITLE));
             String type = bundle.getString(JPushInterface.EXTRA_TITLE);
 
-
             if (type.equals("zhipai")) {
+                try {
+                    mp.setDataSource(context, Uri.parse("android.resource://" + context.getPackageName() + "/" + R.raw.notify));
+                    mp.prepare();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                mp.start();
 //                Intent intentGrab = new Intent(context, GrabOrderActivity.class);
                 Intent intentGrab = new Intent(context, GrabOrderActivity2.class);
                 intentGrab.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -72,15 +81,23 @@ public class MyReceiver extends BroadcastReceiver {
                 intent3.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 context.startActivity(intent3);
             }
-            processCustomMessage(context, bundle);
+//            processCustomMessage(context, bundle);
+
+            /*原本放在第一位*/
+        } else if (JPushInterface.ACTION_REGISTRATION_ID.equals(intent.getAction())) {
+            String regId = bundle.getString(JPushInterface.EXTRA_REGISTRATION_ID);
+            Log.d(TAG, "[MyReceiverACTION_REGISTRATION_ID] 接收Registration Id : " + regId);
+            //保存registrationId:160a3797c80a1a27a63
+            RememberHelper.saveRegistrationId(regId);
+            //send the Registration Id to your server...
 
         } else if (JPushInterface.ACTION_NOTIFICATION_RECEIVED.equals(intent.getAction())) {
-            Log.d(TAG, "[MyReceiver] 接收到推送下来的通知");
+            Log.d(TAG, "[MyReceiverACTION_NOTIFICATION_RECEIVED] 接收到推送下来的通知");
             int notifactionId = bundle.getInt(JPushInterface.EXTRA_NOTIFICATION_ID);
-            Log.d(TAG, "[MyReceiver] 接收到推送下来的通知的ID: " + notifactionId);
+            Log.d(TAG, "[MyReceiverACTION_NOTIFICATION_RECEIVED] 接收到推送下来的通知的ID: " + notifactionId);
 
         } else if (JPushInterface.ACTION_NOTIFICATION_OPENED.equals(intent.getAction())) {
-            Log.d(TAG, "[MyReceiver] 用户点击打开了通知");
+            Log.d(TAG, "[MyReceiverACTION_NOTIFICATION_OPENED] 用户点击打开了通知");
 //          //打开自定义的Activity
             /*Intent i = new Intent(context, MainActivity.class);
             i.putExtras(bundle);
@@ -89,14 +106,14 @@ public class MyReceiver extends BroadcastReceiver {
             context.startActivity(i);*/
 
         } else if (JPushInterface.ACTION_RICHPUSH_CALLBACK.equals(intent.getAction())) {
-            Log.d(TAG, "[MyReceiver] 用户收到到RICH PUSH CALLBACK: " + bundle.getString(JPushInterface.EXTRA_EXTRA));
+            Log.d(TAG, "[MyReceiverACTION_RICHPUSH_CALLBACK] 用户收到到RICH PUSH CALLBACK: " + bundle.getString(JPushInterface.EXTRA_EXTRA));
             //在这里根据 JPushInterface.EXTRA_EXTRA 的内容处理代码，比如打开新的Activity， 打开一个网页等..
 
         } else if (JPushInterface.ACTION_CONNECTION_CHANGE.equals(intent.getAction())) {
             boolean connected = intent.getBooleanExtra(JPushInterface.EXTRA_CONNECTION_CHANGE, false);
-            Log.w(TAG, "[MyReceiver]" + intent.getAction() + " connected state change to " + connected);
+            Log.w(TAG, "[MyReceiverACTION_CONNECTION_CHANGE]" + intent.getAction() + " connected state change to " + connected);
         } else {
-            Log.d(TAG, "[MyReceiver] Unhandled intent - " + intent.getAction());
+            Log.d(TAG, "[MyReceiverACTION_CONNECTION_CHANGE] Unhandled intent - " + intent.getAction());
         }
     }
 
@@ -136,11 +153,11 @@ public class MyReceiver extends BroadcastReceiver {
 
     //send msg to MainActivity
     private void processCustomMessage(Context context, Bundle bundle) {
-        if (MainActivity.isForeground) {
+        /*if (MainActivity.isForeground) {
             String message = bundle.getString(JPushInterface.EXTRA_MESSAGE);
             String extras = bundle.getString(JPushInterface.EXTRA_EXTRA);
-            Log.d("自定义消息", message);
-            Log.d("自定义消息", extras);
+//            Log.d("自定义消息", message);
+//            Log.d("自定义消息", extras);
             Intent msgIntent = new Intent(MainActivity.MESSAGE_RECEIVED_ACTION);
             msgIntent.putExtra(MainActivity.KEY_MESSAGE, message);
             if (!ExampleUtil.isEmpty(extras)) {
@@ -155,6 +172,6 @@ public class MyReceiver extends BroadcastReceiver {
 
             }
             context.sendBroadcast(msgIntent);
-        }
+        }*/
     }
 }
